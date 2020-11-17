553e74335429d743768ee0ffc010faa3b3f28644
Leah Arden <leaharden175@gmail.com>
2020-11-17 13:17:17 -0500

    watering message complete

--- src/components/WateringAlert/WateringAlert.js
+++ src/components/WateringAlert/WateringAlert.js
@@ -2,24 +2,35 @@ import React, { Component } from "react";
 import "./WateringAlert.css";
 
 export default class WateringAlert extends Component {
-
   wateringMessage() {
-    const plant_id = this.props.plants.id
-    const lastWatered = new Date(this.props.plants.date_last_watered)
-    const daysBetween = this.props.plants.days_between_watering
-    const currentDate = new Date()
-    const diffMilliseconds = currentDate - lastWatered
-    const diffDays = Math.floor(diffMilliseconds/(24*60*60*1000))
-    return diffDays
-    }
+    const lastWatered = new Date(this.props.plants.date_last_watered);
+    const daysBetween = this.props.plants.days_between_watering;
+    const currentDate = new Date();
+    const diffMilliseconds = currentDate - lastWatered;
+    const diffDays = Math.floor(diffMilliseconds / (24 * 60 * 60 * 1000));
+    const overDue = daysBetween + 30;
 
+    if (diffDays < daysBetween) {
+      return (
+        <p>Happy plant</p>
+      ) 
+    } else if (diffDays > overDue){
+      return (
+        <p>Why havent you watered me? It's been {diffDays} days!</p>
+      ) 
+    } else if (diffDays === daysBetween){
+      return (
+        <p>Time to water me</p>
+      )
+    } else {
+      return (
+        <p>You are {diffDays} days late watering me</p>
+      ) 
+    }
+  }
 
   render() {
-    console.log('PROPS', this.props)
-    return (
-      <p>
-        {this.wateringMessage()}
-      </p>
-    )
+    console.log("PROPS", this.props);
+    return <div>{this.wateringMessage()}</div>;
   }
 }
